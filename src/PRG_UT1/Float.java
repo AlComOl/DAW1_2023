1e59e986595c88cf1d9e65988934893c5e0e2687
unknown <alcomol>
2023-09-20 12:24:26 +0200

    Clase_20-01-23

---
+++ src/PRG_UT1/Float.java
@@ -0,0 +1,23 @@
+package PRG_UT1;
+
+public class Float {
+
+	public static void main(String[] args) {
+		float unFloat=0.17f;
+		double unDouble =-0.175;
+		double otroDouble = 502E8;
+		  
+		char a ='\b';
+		
+		System.out.println(a + "g");
+		
+		System.out.println("\t\t \'Hola\"");
+	
+		
+		 System.out.println(unDouble);
+		System.out.println(unFloat);
+		System.out.println(otroDouble);
+
+	}
+
+}
