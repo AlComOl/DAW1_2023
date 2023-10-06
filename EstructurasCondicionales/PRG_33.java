358cfb81520e9cf8f874400efaa8e54a022f06bc
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-06 22:25:25 +0200

    Viernes noche pc casa

--- EstructurasCondicionales/PRG_33.java
+++ EstructurasCondicionales/PRG_33.java
@@ -1,18 +1,20 @@
 package EstructurasCondicionales;
+
+import java.util.Scanner;
+
 /*****************************************************************
 
 Autor: Álvaro Comenge
 
 Fecha:6/10/23
 
-Descripción:33. Algoritmo que lee tres números cualesquiera y los escribe ordenados de forma descendente.
+Descripción:3. Algoritmo que lee tres números cualesquiera y los escribe ordenados de forma ascendente.
 
 ******************************************************************/
-import java.util.*;
-
-public class PRG_3 {
+public class PRG_33 {
 
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
 		
 		
@@ -27,17 +29,17 @@ public class PRG_3 {
 		
 		
 		 if (n < n1 && n < n2 && n1 < n2) { //123
-			System.out.println("Los numeros introducidos en orden ascendente "+n2+","+n1+","+n);
+			System.out.println("Los numeros introducidos en orden ascendente "+n+","+n1+","+n2);
 		 }else if (n < n1 && n < n2 && n1 > n2) { //132
-			 System.out.println("Los numeros introducidos en orden ascendente "+n1+","+n2+","+n); 
+			 System.out.println("Los numeros introducidos en orden ascendente "+n+","+n2+","+n1); 
 		 }else if (n > n1 && n < n2 && n1 < n2) { //213
-			 System.out.println("Los numeros introducidos en orden ascendente "+n2+","+n+","+n1);
-		 }else if (n < n1 && n > n2 && n1 > n2) {//231
 			 System.out.println("Los numeros introducidos en orden ascendente "+n1+","+n+","+n2);
+		 }else if (n < n1 && n > n2 && n1 > n2) {//231
+			 System.out.println("Los numeros introducidos en orden ascendente "+n2+","+n+","+n1);
 		 }else if (n > n1 && n > n2 && n1 > n2) {//321
-			 System.out.println("Los numeros introducidos en orden ascendente "+n+","+n1+","+n2);
-		 }else if (n > n1 && n1 < n2 && n2 < n) {//312
-			 System.out.println("Los numeros introducidos en orden ascendente "+n+","+n2+","+n1);
+			 System.out.println("Los numeros introducidos en orden ascendente "+n2+","+n1+","+n);
+		 }else if (n > n1 && n1 < n2 && n2 < n) {//312b
+			 System.out.println("Los numeros introducidos en orden ascendente "+n1+","+n2+","+n);
 		 }else {
 			 
 			 System.out.println("Hay numeros  que son iguales");
@@ -45,5 +47,4 @@ public class PRG_3 {
 			 entrada.close();
 		 }
 	}
-
 }
