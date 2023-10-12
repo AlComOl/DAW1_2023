06d371a7e74e84b185e7eec4d58e88699bc954cc
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-12 18:21:59 +0200

    58 funciona pero no esta terminado

--- EstructurasCondicionales/PRG_58.java
+++ EstructurasCondicionales/PRG_58.java
@@ -21,22 +21,33 @@ import java.util.*;
 	public class PRG_58 {
 	    public static void main(String[] args) {
 	        Scanner entrada = new Scanner(System.in);
-	       int n,aux=0, cont=0,media;
-	       
-	       
+	       int   porApro=0,porApro1, porSu=0,porSu1 ,aprovados=0;
+	   	   double n=0,aux=0,media=0,suspendidos=0,contPers=-1;
 	       do {
-	    	  System.out.println("Introduce notas:");
-	    	  n=entrada.nextInt();
-	    	  if(n<0) {
-	    		  media= aux/cont;
-	    		  System.out.println(media);
-	    	  }else {
-	    	  aux+=n;
-	    	  cont++;
-	    	  }
-	    	  }while(n>0 && n<10);
-	       		
-	         	
+	    	   System.out.println("Introduce notas");
+	    	   n=entrada.nextDouble();
+	    	   contPers++;
+	    	   if(n>=0&&n<=10) {
+	    		  aux+=n; 
+	    	   }
+	    	   if(n>=5) {
+	    		   aprovados++;
+	    		  
+	    	   }if(n<5){
+	    		   suspendidos++;
+	    		
+	    	   }
+	    	   System.out.println(aux);
+	    	
+	       }while(n != -1);
+	       		media=aux/contPers;
+	       		System.out.println(media);
+	       		porApro1=(int) (aprovados*100/contPers);
+	       		porSu1=(int) (porSu*10/(contPers));
+	       		System.out.println("La media de las notas es :"+ media);
+	       		System.out.println("Hay "+ porApro+"examenes  aprovados que corresponde a un "+porApro1);
+	       		System.out.println("Hay "+ porSu+"examenes  aprovados que corresponde a un "+porSu1);
+	    	   
 	       }
 	      
 	    }
