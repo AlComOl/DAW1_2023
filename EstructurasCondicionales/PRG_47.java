dc71d4c76b31e4453fc20c8f45a90eac9e915ff8
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-17 22:43:16 +0200

    47 terminado

--- EstructurasCondicionales/PRG_47.java
+++ EstructurasCondicionales/PRG_47.java
@@ -1,5 +1,5 @@
 package EstructurasCondicionales;
-<<<<<<< HEAD
+
 /*******************************************************************************************************************
 
 Autor: Álvaro Comenge
@@ -16,27 +16,26 @@ import java.util.*;
 public class PRG_47 {
 	public static void main(String[] args) {
 		 Scanner entrada = new Scanner(System.in);
-		 int n,i,res,aux=0;
-		
-			System.out.println("Introduce un numero entero y positivo");
-			n=entrada.nextInt();
-		 do {
-			if(n<0) {
-				System.out.println("El numero es negativo");
-			}else {
-			for(i=1;n>=i;i++) {
-	
-				if(n%i==0) {
-					res=n/i;
-					aux+=res;//acumulador
-				}
-				}
-			System.out.println(aux);
-			}
-			 } while (n<0);
-		     
-		 
-		
+		 int n,i,aux=0;
+			do {
+				System.out.println("Introduce un numero entero y positivo");
+					n=entrada.nextInt();
+					while(n<0);
+//						if(n<0) {
+//							
+//						}else {
+							for(i=1;n>=i;i++) {
+								if(n%i==0) {
+									aux+=i;//acumulador
+								}
+							}
+//						}
+						System.out.println("La suma de los divisores es:"+aux);
+						aux=0;
+			} while (n>0); 
+			
+	}
+}
 		
 		 
 		 
@@ -53,40 +52,53 @@ public class PRG_47 {
 //		    }
 //
 //		    System.out.println("La suma de los divisores de " + n + " es " + o);
-	}
-}
-=======
+//	}
+//}
 
 
+//	public class PRG_47 {
+//
+//		public static void main(String[] args) {
+//			Scanner entrada=new Scanner(System.in);
+//			int i,n,Suma=0,aux=0;
+//			//creo que el bucle while es el mas optimo, auque para hacer
+//			do {
+//				System.out.println("Introduce un numero");
+//				n=entrada.nextInt();
+//	
+//				for(i=1;n>=i;i++) {
+//					if(n%i==0) {
+//						aux+=i;//acumulador
+//					}
+//					}
+//				System.out.println(""+aux);
+//				aux=0;
+//			} while (n>0); 
+//			
+//				
+//			
+//	}
+//}
 
-	import java.util.*;
-	/*******************************************************************************************************
 
-	Autor: Álvaro Comenge
-
-	Fecha:17/10/23
-
-
-		Descripción:
-
-		Algoritmo que lee un número entero y positivo N y calcula y escribe la suma de sus divisores.
-		
-	********************************************************************************************************/
-	public class PRG_47 {
-
-		public static void main(String[] args) {
-			Scanner entrada=new Scanner(System.in);
-			int n,Suma=0;
-			
-			do {
-				Suma+=0;
-				System.out.println("Introduce un numero");
-				n=entrada.nextInt();
-			} while (n>0); 
-			System.out.println(""+Suma);
-				
-			
-		}
-
-	}
->>>>>>> b48c777dc64498bab87c1129894cf90ce76aac81
+//public class PRG_47 {
+//	public static void main(String[] args) {
+//		 Scanner entrada = new Scanner(System.in);
+//		 int n,i,res,aux=0;
+//		
+//			System.out.println("Introduce un numero entero y positivo");
+//			n=entrada.nextInt();
+//		
+//			if(n<0) {
+//				System.out.println("El numero es negativo");
+//			}else {
+//			for(i=1;n>=i;i++) {
+//				if(n%i==0) {
+//					aux+=i;//acumulador
+//				}
+//				}
+//			System.out.println("La suma de los divissores de : "+n+" es : "+aux);
+//			aux=0;
+//			}
+//	}
+//}
