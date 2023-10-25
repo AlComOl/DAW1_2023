419d91ec7b3cc2f3ebfd28a55034d61ee01e7a42
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-25 22:36:38 +0200

    BoleanparInpar

---
+++ src/EjerciciosClase/src/BolParInpar.java
@@ -0,0 +1,29 @@
+package src.EjerciciosClase.src;
+
+
+	import java.util.Scanner;
+
+	public class BolParInpar {
+
+		public static void main(String[] args) {
+			Scanner entrada=new Scanner(System.in);
+			int pares=0, impares=0;
+			boolean par=false;
+			
+			for (int i=1;i<=100;i++){
+				if(par) {
+					pares+=i;
+					
+				}else { 
+					impares+=i;
+				}
+				par=!par;
+			}
+				
+			System.out.println("suma pares"+pares);
+			System.out.println("suma impares"+impares);
+		}
+
+
+	}
+
