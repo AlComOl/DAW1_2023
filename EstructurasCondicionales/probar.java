30fa2c25edd56ebf4b9d5ed6c695aff1de718e62
AlComOl <alvcomoli@alu.edu.gva.es>
2023-10-29 00:35:49 +0200

    bona nit

--- EstructurasCondicionales/probar.java
+++ EstructurasCondicionales/probar.java
@@ -2,21 +2,49 @@ package EstructurasCondicionales;
 import java.util.*;
 
 public class probar {
-    public static void main(String[] args) {
-        Scanner tec = new Scanner(System.in);
-        int num, div = 2;
+//    public static void main(String[] args) {
+//        Scanner tec = new Scanner(System.in);
+//        int n,m,i,acumula=0;
+//
+//        System.out.println("Introduce el numero");
+//        n = tec.nextInt();
+//        System.out.println("Introduce el numero");
+//        m = tec.nextInt();
+////        if(n<m) {
+////        	n=m;
+////        	m=n;
+////        }
+//        acumula=n;
+//        for(i=0;n<m;i++) {
+//        	n-=m;//acumula=acumula-m
+//        System.out.println(acumula);
+//        }
+//        System.out.println(i);
+//       
+//    }
+//}
 
-        System.out.print("Introduce el numero a descomponer factorialmente: ");
-        num = tec.nextInt();
-
-        System.out.print("Los factores del numero " + num + " son: ");
-        while(num != 1) {
-
-            while(num % div == 0){
-                System.out.print(div + ", ");
-                num = num / div;
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		int n,igual=0,mayor=0,menor=0,caracter=0;
+		
+	  
+	
+            for(int i=10;i>0;i--) {
+            	System.out.println("Introduce el numero");	
+            	n=entrada.nextInt();
+            	
+            	if(n==0) {
+            		igual++;
+            	}else if(n<0) {
+            		menor++;
+            	}else if(n>0){
+            		mayor++;
+            	}else {
+            		caracter++;
+            	}
             }
-                div++;
-        }
-    }
-}
+            System.out.println("Hay "+igual+" numeros igules "+mayor +" numeros mayores "+menor+" numeros menores que 0 , y caracteres mal introducidos "+caracter);
+            entrada.close();
+                  }
+      }
