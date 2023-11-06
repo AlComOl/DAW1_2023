0ff3b78399061ce469b4b8c3665bc5c5369c301f
AlComOl <alvcomoli@alu.edu.gva.es>
2023-11-06 16:08:28 +0100

    actualizacion clase temperatura

--- POO/Numero.java
+++ POO/Numero.java
@@ -1,6 +1,26 @@
 package POO;
 import java.util.*;
+/****************************************************************************************************************************
 
+Autor: Álvaro Comenge
+
+Fecha:05/11/23
+
+
+Descripción:
+	Realiza una clase Número que almacene un número entero y tenga las siguientes características: 
+	• Constructor por defecto que inicializa a 0 el número interno. 
+	• Constructor que inicializa el número interno. 
+	• Método aniade que permite sumarle un número al valor interno.
+	• Método resta que resta un número al valor interno.
+ 	• Método getValor. Devuelve el valor interno. 
+	• Método getDoble. Devuelve el doble del valor interno.
+	• Método getTriple. Devuelve el triple del valor interno. • Método setNumero. Inicializa de nuevo el valor interno.
+
+
+
+
+***********************************************************************************************************************************/
 public class Numero {
 	//Atributos
 	
---
+++ POO/TemperaturaNew.java
@@ -0,0 +1,61 @@
+package POO;
+import java.util.*;
+/***************************************************************************
+ *@author acome
+ *
+ 	Autor: Álvaro Comenge
+
+	Fecha: 1/11/23
+	
+	Descripción:
+	
+		Realice una clase Temperatura, la cual convierta grados Celsius a Farenheit y 
+		viceversa. Para ello cree dos métodos double celsiusToFarenheit(double) y 
+		farenheitToCelsius(double). Pruebe dicha clase.
+
+ **************************************************************************/
+
+public class TemperaturaNew {
+	//Atributos
+	private double temperatura;
+//	private String escala;
+	
+	//CONSTRUCTOR
+	public TemperaturaNew(double valor, String tipo) {
+		if(tipo.equals("Celsius")) {
+			System.out.println(((valor*9/5)+32)+" Farenheit");
+		}else if(tipo.equals("Farenheit")) {
+			System.err.println((farenheitToCelsius()+"Celsius"));
+		}
+		temperatura=valor;
+
+	}
+	//METODOS
+
+	public double farenheitToCelsius() {
+		
+		return  (temperatura-32)*5/9; 
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		int cantidad=0;
+		String tipo="";
+		
+		
+		System.out.println("Introduce Temperatura");
+		cantidad=entrada.nextInt();
+		System.out.println("Introduce Escala");
+		tipo=entrada.next();
+		
+		
+		TemperaturaNew a1=new TemperaturaNew(cantidad,tipo);
+	
+	
+		
+
+		entrada.close();
+	}
+
+}
