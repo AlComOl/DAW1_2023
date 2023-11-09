9df466c8b82531b9d76ad9d0651bb96a37f10d5b
AlComOl <alvcomoli@alu.edu.gva.es>
2023-11-09 17:12:26 +0100

    Clase Peso

---
+++ POO/Cosumo.java
@@ -0,0 +1,64 @@
+package POO;
+import java.util.*;
+/********************************************************************************************************************************************************************************************************************************************************************
+
+Autor: Álvaro Comenge
+
+Fecha: 8/11/23
+
+Descripción:
+	Implementa una clase Consumo, la cual forma parte de la centralita electrónica de un coche y tiene las siguientes características: 
+	• Atributos: 
+	– kms. Kilómetros recorridos por el coche. 
+	– litros. Litros de combustible consumido. 
+	– vmed. Velocidad media. 
+	– pgas. Precio de la gasolina. 
+	• Métodos: 
+	– getTiempo. Indicará el tiempo empleado en realizar el viaje. 
+	– consumoMedio. Consumo medio del vehículo (en litros cada 100 kilómetros). 
+	– consumoEuros. Consumo medio del vehículo (en euros cada 100 kilómetros).
+	 No olvides crear un constructor para la clase que establezca el valor de los atributos. Elige el tipo de datos más apropiado para cada atributo.
+
+************************************************************************************************************************************************************************************************************************************************************************/
+public class Cosumo {
+//	Atributos
+	private double kmsRecorridos;
+	private double litrosConsumidos;
+	private double vMed;
+	private double pGas;
+//	Constructor
+	public Cosumo(double kms,double litros, double vMed, double pGas) {
+		this.kmsRecorridos=kms;
+		this.litrosConsumidos=litros;
+		this.vMed=vMed;
+		this.pGas=pGas;
+	}
+//	Metodos
+	public double getTiempo() {
+		
+		return kmsRecorridos/vMed;
+		
+	}
+	public double consumoMedio() {
+		
+		return litrosConsumidos/100;
+	}
+	public double consumoEuros() {
+		
+		return (kmsRecorridos*litrosConsumidos)/100;
+	}
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		Cosumo consumoAudi = new Cosumo(100,20,100,1.70);
+		
+		System.out.println(consumoAudi.getTiempo()+" horas");
+		System.out.println(consumoAudi.consumoMedio()+" l a los 100 kilometros");
+		System.out.println(consumoAudi.consumoEuros()+" Euros a los 100 Kilometros");
+		
+		
+
+	}
+
+}
--- POO/Peso.java
+++ POO/Peso.java
@@ -1,10 +1,114 @@
 package POO;
+import java.util.*;
+/********************************************************************************************************************************************************************************************************************************************************************
 
+Autor: Álvaro Comenge
+
+Fecha: 8/11/23
+
+Descripción:
+	Crea la clase Peso, la cual tendrá las siguientes características: 
+	 • Deberá tener un atributo donde se almacene el peso de un objeto en kilogramos.
+	 • En el constructor se le pasará el peso y la medida en la que se ha tomado (‘Lb’ para libras, ‘Li’ para lingotes, ‘Oz’ para onzas, ‘P’ para peniques, ‘K’ para kilos, ‘G’ para gramos y ‘Q’ para quintales).
+	 • Deberá de tener los siguientes métodos: 
+	 – getLibras. Devuelve el peso en libras. 
+	 – getLingotes. Devuelve el peso en lingotes. 
+	 – getPeso. Devuelve el peso en la medida que se pase como parámetro (‘Lb’ para libras, ‘Li’ para lingotes, ‘Oz’ para onzas, ‘P’ para peniques, ‘K’ para kilos, ‘G’ para gramos y ‘Q’ para quintales). 
+	 Para la realización del ejercicio toma como referencia los siguientes datos: 1 Libra = 16 onzas = 453 gramos. 1 Lingote = 32,17 libras = 14,59 kg. 1 Onza = 0,0625 libras = 28,35 gramos. 1 Penique = 0,05 onzas = 1,55 gramos. 1 Quintal = 100 libras = 43,3 kg.
+	 Crea además un método main para testear y verificar los métodos de esta clase.
+
+
+************************************************************************************************************************************************************************************************************************************************************************/
 public class Peso {
-
+//	Atributos
+	
+	private double Kilogramo;
+	
+//	Constructor //Introducimos el peso y la medida en el constructor conviertiendolo a KG 
+//	independientemente de la unidad en la que este y guardandomos en Kilogramos
+	private Peso(double peso, String medida) {
+		
+				Kilogramo=peso;
+		//El condicional si es igual a la medida lo convierte a kilogramos guardandolo en el Atributo
+			if(medida.equalsIgnoreCase("Lb")) {
+				Kilogramo=peso*0.453;
+			}
+			if(medida.equalsIgnoreCase("Li")) {
+				Kilogramo=peso*14.59;
+			}
+			if(medida.equalsIgnoreCase("Oz")) {
+				Kilogramo=peso*0.2835;
+			}
+			if(medida.equalsIgnoreCase("P")) {
+				Kilogramo=peso*0.00155;
+			}
+			if(medida.equalsIgnoreCase("Q")) {
+				Kilogramo=peso*43.3;
+			}
+			if(medida.equalsIgnoreCase("K")) {
+				Kilogramo=peso;
+			}
+			if(medida.equalsIgnoreCase("G")) {
+				Kilogramo=peso/1000;
+			}
+			
+	}
+			
+//			Metodo
+//			al dividir los Kilogramos obtenidos entre el equibalente en libras nos convierte a libras.
+			public double getLibras() {
+				
+				return Kilogramo/0.453;
+		
+			}
+//			al dividir los Kilogramos obtenidos entre el equibalente en lingotes nos convierte a lingotes.
+			public double getLingotes() {
+				return Kilogramo/14.59;
+			}
+//			con un switch validamos la unidad y lo convertimos en el metodo
+			public double getPeso(String medida) {
+				 double valor=0;
+				switch (medida) {
+				case "Lb":
+					valor=Kilogramo/0.453;
+					break;
+				case "Li":
+					valor=Kilogramo/14.59;
+					break;
+				case "Oz":
+					valor=Kilogramo/0.02835;
+					break;
+				case "P":
+					valor=Kilogramo/0.0155;
+					break;
+				case "Q":
+					valor=Kilogramo/43.3;
+					break;
+				case "K":
+					valor=Kilogramo;
+					break;
+				case "G":
+					valor=Kilogramo/0.001;
+					break;
+			
+				default:
+					System.out.println("Esta medida no esta configurada");
+					break;
+				}
+				return 	valor;
+				
+			}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Peso c1=new Peso(1,"Lb");
+//		comprobamos  paso a libras
+		System.out.println(c1.getLibras()+" Libras");
+//		comprobamos	 paso a lingotes	
+		System.out.println(c1.getLingotes()+" Lingotes");
+//		convertimos al peso introducido
+		System.out.println(c1.getPeso("Lb"));
+		
+		
 	}
 
 }
