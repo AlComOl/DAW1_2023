1148b2a00ebdb2b7409507130d463f9efdcc33fe
AlComOl <alvcomoli@alu.edu.gva.es>
2023-11-20 18:29:11 +0100

    char

---
+++ src/EjerciciosClase/src/Char.java
@@ -0,0 +1,17 @@
+package EjerciciosClase.src;
+import java.io.IOException;
+import java.util.*;
+
+public class Char   {
+
+	public static void main(String[] args) throws IOException {
+		Scanner entrada=new Scanner(System.in);
+		char a, b;
+		
+		System.out.println("Introduce el primer caracter");
+		
+		a=(char)System.in.read();
+
+	}
+
+}
