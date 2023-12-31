d542cb36075b4d70b3148706a150c49a4c23c449
AlComOl <alvcomoli@alu.edu.gva.es>
2023-12-31 17:33:55 +0100

    Feliz año 2024

--- src/Refuerzo/Refuerzo3.java
+++ src/Refuerzo/Refuerzo3.java
@@ -14,9 +14,21 @@ public class Refuerzo3 {
 //		ejercicio3_2();
 		
 		//	Ejercicio3
+//		System.out.println("Precio real");
+//		int	 pr=sc.nextInt();
+//		System.out.println("Cuanto puedes llegar a pagar");
+//		int	 pp=sc.nextInt();
+//		
+//		ejercicio3_3(pr,pp);
 		
-		ejercicio3_3(60,24);
-
+//		ejercicio 4 
+		
+//		ejercicio3_4();
+//		ejercicio5
+		
+		System.out.println("Introduce el numero");
+		int numero=sc.nextInt();
+		ejercicio3_5(numero);
 	}
 	
 
@@ -42,41 +54,67 @@ public class Refuerzo3 {
 	
 	
 	public static void ejercicio3_2() {
-		int numero;
+		int numero=0;
 		int aux=0,res;
-		System.out.println("Introduce un numero de 4 cifras");
+		do {
+			System.out.println("Introduce un numero de 4 cifras");
 			numero=sc.nextInt();
+//			filtra que el numero tenga 4 cifras
+		}while(numero<=1000&&numero<=9999) ;	
+//			si se cumple la condición descompone el numero
 			aux=numero;
-			do {
-				res=aux%10;
-		 
-				aux/=10;
-		
-				System.out.println(res);
-		
-			}while(aux>0) ;	
+				while(aux>0) {
+					res=aux%10;
+					aux/=10;
+					System.out.println(res);
+				}
+			
+			
+			
 	}
-		
+			
+//	
 	
 	public static void ejercicio3_3(int pr, int pp) {
 		double cantidadDescontada;
-		int porcentajeDescuento , ct;
-		System.out.println("Precio real");
-		 pr=sc.nextInt();
-		System.out.println("Cuanto puedes llegar a pagar");
-		 pp=sc.nextInt();
+		int porcentajeDescuento;
+		
 		 
 		 cantidadDescontada=pr-pp;
 		 
 		 porcentajeDescuento=(int) ((cantidadDescontada/pr)*100);
 		 System.out.println(porcentajeDescuento);
-		 ct=100-porcentajeDescuento;
-		 
+		
+	 	 
 		 System.out.println("Necesitas una oferta del "+ porcentajeDescuento+" %");
 		 
 		 
 	}
-		
+	
+	public static void  ejercicio3_4() {
+		int n,nc,aux=0;
+		System.out.println("Introduce un numero");
+		 n=sc.nextInt();
+		 nc=n;		
+		 while(nc>0) {
+			aux=nc%10;
+			nc/=10;
+			System.out.println(aux);
+			
+		}
+		 
+	}
+	public static void ejercicio3_5(int param) {
+		 int aux,n,acumula=0;
+		 n=param;
+		 
+		 while(n>0) {
+			 aux=n%10;
+			 acumula+=aux;
+			 n/=10;
+		 }
+		 System.out.println(acumula);
+	 }	
 		
 	}
 	
